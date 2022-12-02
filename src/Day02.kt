47ff90f8437d09382b37238fbebd861bceabfa7f
Robin Pokorny <robin.pokorny@klarna.com>
2022-12-02 23:48:52 +0100

    :zap: refactor day 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,3 +1,30 @@
+/* Move - enum and utils */
+enum class Move {
+    ROCK, PAPER, SCISSORS;
+}
+
+private fun scoreMove(mine: Move) = when (mine) {
+    Move.ROCK -> 1
+    Move.PAPER -> 2
+    Move.SCISSORS -> 3
+}
+
+private fun toMove(input: String) = when (input) {
+    "A", "X" -> Move.ROCK
+    "B", "Y" -> Move.PAPER
+    "C", "Z" -> Move.SCISSORS
+    else -> throw Exception("Unsupported move")
+}
+
+private val winningRounds = mapOf(
+    Move.ROCK to Move.PAPER,
+    Move.PAPER to Move.SCISSORS,
+    Move.SCISSORS to Move.ROCK
+)
+
+private val losingRounds = winningRounds.entries.associate { (k, v) -> v to k }
+
+/* Outcome - enum and utils */
 enum class Outcome {
     WIN, LOSE, DRAW
 }
@@ -15,53 +42,32 @@ private fun toOutcome(input: String) = when (input) {
     else -> throw Exception("Unsupported outcome")
 }
 
-enum class Move {
-    ROCK, PAPER, SCISSORS
-}
-
-private fun scoreMove(mine: Move) = when (mine) {
-    Move.ROCK -> 1
-    Move.PAPER -> 2
-    Move.SCISSORS -> 3
-}
-
-private fun toMove(input: String) = when (input) {
-    "A", "X" -> Move.ROCK
-    "B", "Y" -> Move.PAPER
-    "C", "Z" -> Move.SCISSORS
-    else -> throw Exception("Unsupported move")
-}
-
-private fun parse(input: List<String>) = input
+/* Parse input */
+private fun parse(input: List<String>): List<Pair<String, String>> = input
     .map { it.split(" ") }
     .map { it[0] to it[1] }
 
-private val winningRounds = mapOf(
-    Move.ROCK to Move.PAPER,
-    Move.PAPER to Move.SCISSORS,
-    Move.SCISSORS to Move.ROCK
-)
-
-private val losingRounds = winningRounds.entries.associate { (k, v) -> v to k }
-
-private fun scoreRound1(round: Pair<Move, Move>): Int {
+/* Part 1 */
+private fun scoreRoundMoveMove(round: Pair<Move, Move>): Int {
     val (theirs, mine) = round
 
-    val outcome =
-        if (theirs == mine) Outcome.DRAW
-        else if (winningRounds[theirs] == mine) Outcome.WIN
-        else Outcome.LOSE
+    val outcome = when (mine) {
+        theirs -> Outcome.DRAW
+        winningRounds[theirs] -> Outcome.WIN
+        else -> Outcome.LOSE
+    }
 
     return scoreMove(mine) + scoreOutcome(outcome)
 }
 
-private fun calcTotalScore1(rounds: List<Pair<String, String>>): Int = rounds
-    .map { (first, second) -> toMove(first) to toMove(second) }
-    .map(::scoreRound1)
-    .sum()
+private fun calcTotalScoreMoveMove(rounds: List<Pair<String, String>>): Int =
+    rounds
+        .map { (first, second) -> toMove(first) to toMove(second) }
+        .map(::scoreRoundMoveMove)
+        .sum()
 
-
-private fun scoreRound2(round: Pair<Move, Outcome>): Int {
+/* Part 2 */
+private fun scoreRoundMoveOutcome(round: Pair<Move, Outcome>): Int {
     val (theirs, outcome) = round
 
     val mine: Move = when (outcome) {
@@ -73,22 +79,23 @@ private fun scoreRound2(round: Pair<Move, Outcome>): Int {
     return scoreMove(mine) + scoreOutcome(outcome)
 }
 
-private fun calcTotalScore2(rounds: List<Pair<String, String>>): Int = rounds
-    .map { (first, second) -> toMove(first) to toOutcome(second) }
-    .map(::scoreRound2)
-    .sum()
+private fun calcTotalScoreMoveOutcome(rounds: List<Pair<String, String>>): Int =
+    rounds
+        .map { (first, second) -> toMove(first) to toOutcome(second) }
+        .map(::scoreRoundMoveOutcome)
+        .sum()
 
 fun main() {
     val input = parse(readDayInput(2))
     val testInput = parse(rawTestInput)
 
     // PART 1
-    assertEquals(calcTotalScore1(testInput), 15)
-    println("Part1: ${calcTotalScore1(input)}")
+    assertEquals(calcTotalScoreMoveMove(testInput), 15)
+    println("Part1: ${calcTotalScoreMoveMove(input)}")
 
     // PART 2
-    assertEquals(calcTotalScore2(testInput), 12)
-    println("Part2: ${calcTotalScore2(input)}")
+    assertEquals(calcTotalScoreMoveOutcome(testInput), 12)
+    println("Part2: ${calcTotalScoreMoveOutcome(input)}")
 }
 
 private val rawTestInput = """
