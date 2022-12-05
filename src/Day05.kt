71530a530847e7c4dedde102f0f76809aad65bdd
Robin Pokorny <robin.pokorny@klarna.com>
2022-12-05 18:25:48 +0100

    :zap: day 5, part 1

---
+++ src/Day05.kt
@@ -0,0 +1,70 @@
+typealias Stacks = List<ArrayDeque<Char>>
+typealias Steps = List<Triple<Int, Int, Int>>
+
+private fun parse(input: List<String>): Pair<Stacks, Steps> {
+    val (rawStacks, rawSteps) = input
+        .joinToString("\n")
+        .split("\n\n")
+        .map { it.lines() }
+
+    val stacksPrepared = rawStacks
+        .reversed()
+        .map { it.filterIndexed { index, char -> index % 4 == 1  }.toList() }
+
+    val stacks = stacksPrepared
+        .first()
+        .map { it.digitToInt() - 1 }
+        .map { i -> stacksPrepared.mapNotNull { it.getOrNull(i) }.drop(1).filter { it != ' ' } }
+        .map { ArrayDeque(it) }
+
+    val re = """.* (\d+) .*(\d+).+(\d+).*""".toRegex()
+    val steps = rawSteps
+        .map { re.matchEntire(it)!!.groupValues.drop(1).map(String::toInt) }
+        .map{ Triple(it[0], it[1] - 1, it[2] - 1) }
+
+    return Pair(stacks, steps)
+}
+
+private fun part1(input: Pair<Stacks, Steps>): String {
+    val (stacks, steps) = input
+
+    return steps
+        .fold(stacks){ s, (count, from, to) ->
+            repeat(count) { s[to].addLast(s[from].removeLast()) }
+
+            s
+         }
+        .map { it.last() }
+        .joinToString("")
+}
+
+private fun part2(input: List<String>): Int {
+    return 0
+}
+
+fun main() {
+    val input = parse(readDayInput(5))
+    val testInput = parse(rawTestInput)
+
+    println(input.second[3])
+
+    // PART 1
+    assertEquals(part1(testInput), "CMZ")
+    println("Part1: ${part1(input)}")
+
+    // PART 2
+    // assertEquals(part2(testInput), 0)
+    // println("Part2: ${part2(input)}")
+}
+
+private val rawTestInput = """
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+""".trimIndent().lines()
