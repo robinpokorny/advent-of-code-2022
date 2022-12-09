ecd1c43c416de50b025e9d2f91aa42ab9bd57f54
Robin Pokorny <robin.pokorny@klarna.com>
2022-12-09 14:54:28 +0100

    :zap: day 9, part 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,8 +1,6 @@
 import kotlin.math.sign
 
-typealias Motion = Char
-
-private fun parse(input: List<String>): List<Motion> = input
+private fun parse(input: List<String>): List<Char> = input
     .flatMap {
         val (dir, steps) = it.split(" ")
         List(steps.toInt()) { _ -> dir.single() }
@@ -10,45 +8,57 @@ private fun parse(input: List<String>): List<Motion> = input
 
 data class Point(val x: Int, val y: Int)
 
-private fun countTailPositions(motions: List<Motion>): Int = motions
-    .scan(Point(0, 0)) { head, move ->
-        when (move) {
-            'R' -> head.copy(x = head.x + 1)
-            'L' -> head.copy(x = head.x - 1)
-            'U' -> head.copy(y = head.y - 1)
-            'D' -> head.copy(y = head.y + 1)
-            else -> head // Never
-        }
-    }
-    .scan(Point(0, 0)) { tail, head ->
-        val dx = head.x - tail.x
-        val dy = head.y - tail.y
+private fun moveHead(head: Point, move: Char): Point = when (move) {
+    'R' -> head.copy(x = head.x + 1)
+    'L' -> head.copy(x = head.x - 1)
+    'U' -> head.copy(y = head.y - 1)
+    'D' -> head.copy(y = head.y + 1)
+    else -> head // Never
+}
 
-        val touching = dx in -1..1 && dy in -1..1
+private fun moveTail(tail: Point, head: Point): Point {
+    val dx = head.x - tail.x
+    val dy = head.y - tail.y
 
-        if (touching)
-            tail
-        else
-            Point(tail.x + dx.sign, tail.y + dy.sign)
-    }
+    val touching = dx in -1..1 && dy in -1..1
+
+    if (touching) return tail
+
+    return Point(tail.x + dx.sign, tail.y + dy.sign)
+}
+
+private fun countTailPositions(motions: List<Char>): Int = motions
+    .scan(Point(0, 0), ::moveHead)
+    .scan(Point(0, 0), ::moveTail)
     .toSet()
     .size
 
-private fun part2(input: List<String>): Int {
-    return 0
-}
+private fun count10TailPositions(motions: List<Char>): Int = motions
+    .scan(Point(0, 0), ::moveHead)
+    .scan(Point(0, 0), ::moveTail)
+    .scan(Point(0, 0), ::moveTail)
+    .scan(Point(0, 0), ::moveTail)
+    .scan(Point(0, 0), ::moveTail)
+    .scan(Point(0, 0), ::moveTail)
+    .scan(Point(0, 0), ::moveTail)
+    .scan(Point(0, 0), ::moveTail)
+    .scan(Point(0, 0), ::moveTail)
+    .scan(Point(0, 0), ::moveTail)
+    .toSet()
+    .size
 
 fun main() {
     val input = parse(readDayInput(9))
     val testInput = parse(rawTestInput)
+    val testInput2 = parse(rawTestInput2)
 
     // PART 1
     assertEquals(countTailPositions(testInput), 13)
     println("Part1: ${countTailPositions(input)}")
 
     // PART 2
-    // assertEquals(part2(testInput), 0)
-    // println("Part2: ${part2(input)}")
+    assertEquals(count10TailPositions(testInput2), 36)
+    println("Part2: ${count10TailPositions(input)}")
 }
 
 private val rawTestInput = """
@@ -60,4 +70,15 @@ private val rawTestInput = """
     D 1
     L 5
     R 2
+""".trimIndent().lines()
+
+private val rawTestInput2 = """
+    R 5
+    U 8
+    L 8
+    D 3
+    R 17
+    D 10
+    L 25
+    U 20
 """.trimIndent().lines()
