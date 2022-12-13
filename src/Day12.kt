2a7ef2fb322917761ca8f01888264255218d8df9
Robin Pokorny <robin@luciepokorna.com>
2022-12-13 20:27:42 +0100

    :zap: day 12, part 2

--- src/Day12.kt
+++ src/Day12.kt
@@ -21,8 +21,12 @@ private fun parse(input: List<String>): Triple<Map<Point, Int>, Point, Point> {
     return Triple(map, start, end)
 }
 
-private fun part1(input: Triple<Map<Point, Int>, Point, Point>): Int {
-    val (map, start, end) = input
+private fun shortestPath(
+    map: Map<Point, Int>,
+    start: Point,
+    isEnd: (Point) -> Boolean,
+    isValidPath: (Point, Point) -> Boolean
+): Int {
     val processed = mutableSetOf<Point>()
     val costMap = mutableMapOf<Point, Int>()
     val queue = PriorityQueue<Point> { a, b -> costMap[a]!! - costMap[b]!! }
@@ -34,30 +38,45 @@ private fun part1(input: Triple<Map<Point, Int>, Point, Point>): Int {
     while (queue.isNotEmpty()) {
         val next = queue.remove()
 
-        if (processed.contains(next)) continue
+        if (processed.contains(next)) continue else processed.add(next)
 
-        processed.add(next)
-
-        val neighbors = listOf(
+        val adjacent = listOf(
             next.copy(x = next.x + 1),
             next.copy(x = next.x - 1),
             next.copy(y = next.y - 1),
             next.copy(y = next.y + 1),
         )
-            .filter { it in map && (map[it]!! - map[next]!! <= 1) }
-            .forEach {
-                costMap.set(it, costMap[next]!! + 1)
-                queue.add(it)
-            }
+            .filter { it in map && isValidPath(next, it) }
 
-        costMap[end]?.also { return it }
+        adjacent.forEach {
+            costMap.set(it, costMap[next]!! + 1)
+            queue.add(it)
+        }
+
+        adjacent.find { isEnd(it) }?.also { return costMap[it]!! }
     }
 
     return 0
 }
 
-private fun part2(input: List<String>): Int {
-    return 0
+private fun part1(input: Triple<Map<Point, Int>, Point, Point>): Int {
+    val (map, start, end) = input
+
+    return shortestPath(
+        map,
+        start,
+        { it == end },
+        { from, to -> map[to]!! - map[from]!! <= 1 })
+}
+
+private fun part2(input: Triple<Map<Point, Int>, Point, Point>): Int {
+    val (map, _, end) = input
+
+    return shortestPath(
+        map,
+        end,
+        { map[it]!! == 0 },
+        { from, to -> map[from]!! - map[to]!! <= 1 })
 }
 
 fun main() {
@@ -69,8 +88,8 @@ fun main() {
     println("Part1: ${part1(input)}")
 
     // PART 2
-    // assertEquals(part2(testInput), 0)
-    // println("Part2: ${part2(input)}")
+    assertEquals(part2(testInput), 29)
+    println("Part2: ${part2(input)}")
 }
 
 private val rawTestInput = """
